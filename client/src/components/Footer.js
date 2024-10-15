eebd83a705e662390937b16cab93f06a2a70c75c
1112yash <ygarg1112@gmail.com>
2024-10-15 21:03:00 +0530

    Resume Builder Added

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <p class="footer-heart">
+        <p>Made with <a href="https://1112yash.github.io" target="_blank">
+          Yash Garg
+        </a></p>
+        
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
